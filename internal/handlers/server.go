04acf2f381b418486c7f5f03e4d5f9a8514279a0
infinage <37257700+Infinage@users.noreply.github.com>
2026-05-29 17:23:16 +0530

    Cleanup BG job + refactor II

--- internal/handlers/server.go
+++ internal/handlers/server.go
@@ -2,14 +2,83 @@ package handlers
 
 import (
 	"context"
+	"embed"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
 	paste "github.com/infinage/pastebin/pkg"
 )
 
 type Application struct {
-	st  *paste.Store
-	ctx context.Context
+	st     *paste.Store
+	ctx    context.Context
+	assets embed.FS
+	stop   context.CancelFunc
 }
 
-func NewApplication(ctx context.Context) *Application {
-	return &Application{st: paste.NewEmptyStore(), ctx: ctx}
+func NewApplication(assets embed.FS) *Application {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	return &Application{st: paste.NewEmptyStore(), ctx: ctx, stop: stop, assets: assets}
+}
+
+// Periodically cleanup store and remove stale entries
+func (app *Application) Serve(addr string) error {
+	go app.startBGJobs(5)
+
+	mux := app.routes()
+	server := &http.Server{Addr: addr, Handler: mux}
+	var wg sync.WaitGroup
+
+	// Interrupt is caught by App.ctx, we need to listen for 
+	// int and call shutdown on the server manuallly
+	wg.Go(func() {
+		<-app.ctx.Done() // block until fired
+
+		// Shutdown immediately stops accepting new cons and drops stale ones
+		// Grace duration is for the existing ones - it doesn't close them just returns
+		grace := 3 * time.Second
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), grace)
+		defer cancel()
+
+		server.Shutdown(shutdownCtx)
+	})
+
+	err := server.ListenAndServe()
+	if err != nil {
+		return err
+	}
+
+	wg.Wait()
+	return nil
+}
+
+func (app *Application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", app.HandleHome)
+	mux.HandleFunc("GET /paste", app.HandleNewForm)
+	mux.HandleFunc("GET /paste/{id}", app.HandleGet)
+	mux.HandleFunc("POST /paste", app.HandleInsert)
+	mux.HandleFunc("PUT /paste/{id}", app.HandleUpdate)
+	mux.HandleFunc("DELETE /paste/{id}", app.HandleDelete)
+	mux.Handle("GET /assets/", http.FileServer(http.FS(app.assets)))
+	return mux
+}
+
+// Periodically cleans up the store of stale entries (every x minutes)
+func (app *Application) startBGJobs(minutes int) {
+	ticker := time.NewTicker(time.Minute * time.Duration(minutes))
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-app.ctx.Done():
+			return
+		case <-ticker.C:
+			app.st.Cleanup()
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"context"
 	"embed"
 	"fmt"
-	"net/http"
 
 	"github.com/infinage/pastebin/internal/handlers"
 )
@@ -13,17 +11,8 @@ import (
 var assets embed.FS
 
 func main() {
-	mux := http.NewServeMux()
-	app := handlers.NewApplication(context.Background())
-
-	mux.HandleFunc("GET /", app.HandleHome)
-	mux.HandleFunc("GET /paste", app.HandleNewForm)
-	mux.HandleFunc("GET /paste/{id}", app.HandleGet)
-	mux.HandleFunc("POST /paste", app.HandleInsert)
-	mux.HandleFunc("PUT /paste/{id}", app.HandleUpdate)
-	mux.HandleFunc("DELETE /paste/{id}", app.HandleDelete)
-	mux.Handle("GET /assets/", http.FileServer(http.FS(assets)))
-
-	fmt.Println("Listening on port 8080")
-	http.ListenAndServe(":8080", mux)
+	app := handlers.NewApplication(assets)
+	port := "8080"
+	fmt.Printf("Starting server on port '%v'\n", port)
+	app.Serve(":" + port)
 }
